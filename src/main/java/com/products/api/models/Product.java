5edbdf4d51ebd80c2bd52c5738b16a61f77860d0
Facundo Agustín Fernández <facuassain@gmail.com>
2022-01-25 23:33:22 -0300

    refactor: lombok added to project, setters and getters removed and using @Data annotation instead

--- src/main/java/com/products/api/models/Product.java
+++ src/main/java/com/products/api/models/Product.java
@@ -5,7 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class Product {
 
     @Id
@@ -14,37 +17,4 @@ public class Product {
     private String title;
     private Double price;
     private String thumbnail;
-
-    
-    public String getTitle() {
-        return title;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getThumbnail() {
-        return thumbnail;
-    }
-
-    public void setThumbnail(String thumbnail) {
-        this.thumbnail = thumbnail;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
